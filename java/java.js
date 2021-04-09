4c063e96e62345d896f7d902bb0d8c8ad31aa41e
Yann Melo <yann.melo@gmail.com>
2021-04-08 21:08:55 -0300

    Cardflip + randomize feature + card structure

--- java/java.js
+++ java/java.js
@@ -1,16 +1,33 @@
 let cards;
-const amountofcards = document.querySelector(".cards")
+const amountofcards = document.querySelector(".cards");
+const list = ["bobrossparrot", "explodyparrot", "metalparrot", "fiestaparrot", "revertitparrot", "tripletsparrot", "unicornparrot"]
+let cardgroup = [];
+
 verify()
 
-
 function verify(){
-        cards = prompt("Com quantas cartas deseja jogar?");
-    while (cards < 4 || cards %2 !== 0 || cards > 14 || cards == null){
+    while (cards < 4 || (cards %2) !== 0 || cards > 14 || cards == null){
         cards = (prompt("Com quantas cartas deseja jogar?"));
+    }
+    
+    for(let i=0; i < (cards/2); i++){
+        const ecardlist =
+        `<div onclick='flipcard(this)' class='ecard'> 
+        <img class="back-face" src="Images/front.png">
+        <img class="front-face" src="Images/${list[i]}.gif">
+        </div>`;
+        cardgroup.push(ecardlist);
+        cardgroup.push(ecardlist);
+    }
+    cardgroup.sort(randomize);
+    for(let i=0; i < cards; i++){
+        amountofcards.innerHTML += cardgroup[i];
     }
 }
 
-for(let i=0; i < cards; i++){
-    const ecardlist = "<div class=\"ecard\"></div>";
-    amountofcards.innerHTML += ecardlist
+function randomize() { 
+	return Math.random() - 0.5; 
+}
+function flipcard(flip){
+    flip.classList.toggle('fliped')
 }
